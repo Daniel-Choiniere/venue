50ff054e6fa65bd2d1efd38040a93324d0b9228a
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-01 20:11:48 +0000

    working return ticket dropdown with remove workng, still need to push back to avaliable

--- venue.js
+++ venue.js
@@ -1,7 +1,7 @@
 var venue = {
     name: "TicketMAster",
     ticketsAvaliable: ["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5"],
-    ticketsPurchased: [],
+    ticketsPurchased: ["A6", "A7", "B6", "B7"],
     
     numberOfTicketsAvailable: function() {
         // length of the number of seats available, returned as an integer (1,5,20, etc)
@@ -28,7 +28,28 @@ function selectSeat() {
     ticketPurchasedRemoved.remove(ticketPurchasedRemoved.selectedIndex);
 }
 
+// Select a user clicked specific seat option from a dropdown
+function unreserveSeat() {
 
+    // Get selected seat from user
+    var seatReserved = document.getElementById("mySelectReserved").value;
+
+    // Tests
+    console.log(seatReserved);
+    // console.log(venue.ticketsReserved.indexOf(seatReserved));
+    // Remove selected seat from ticketsAvaliable
+    // Push selected room into ticketsPurchased
+    venue.ticketsAvaliable.splice(venue.ticketsAvaliable.indexOf(seatReserved), 1);
+    venue.ticketsPurchased.push(seatReserved);
+    
+    // Remove selected item from dropdown
+    var removeRemoved = document.getElementById("mySelectReserved");
+    removeRemoved.remove(removeRemoved.selectedIndex);
+}
+
+
+
+// Create dropdown with each item from ticketsAvailable
 var ticketList = "<form> <select id='mySelect'>";
 for (var i=0; i < venue.ticketsAvaliable.length; i++) {
     
@@ -37,4 +58,15 @@ for (var i=0; i < venue.ticketsAvaliable.length; i++) {
 ticketList += "</select> </form>";
 
 document.getElementById("seatsAvail").innerHTML = ticketList;
+
+
     
+// Create a dropdown with each item in ticketsPurchased
+var reservedTicketList = "<form> <select id='mySelectReserved'>";
+for (var i=0; i < venue.ticketsPurchased.length; i++) {
+    
+    reservedTicketList += '<option value="' + venue.ticketsPurchased[i] + '">' + venue.ticketsPurchased[i] + "</option>";
+}
+reservedTicketList += "</select> </form>";
+
+document.getElementById("seatsReserved").innerHTML = reservedTicketList;
